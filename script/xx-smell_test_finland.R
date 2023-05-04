d8c779c36a7179d3abc699006347a9771a904882
Ben Matthews <benmatthewsed@gmail.com>
2023-05-04 14:39:28 +0100

    add population analysis for finland men

--- script/xx-smell_test_finland.R
+++ script/xx-smell_test_finland.R
@@ -3,6 +3,7 @@ library(ggplot2)
 library(tidyr)
 library(purrr)
 library(readr)
+library(stringr)
 
 library(ungroup)
 
@@ -146,3 +147,131 @@ res$goodness.of.fit$standard.errors |>
   as_tibble()
 
 res$deep$H
+
+
+
+# population --------------------------------------------------------------
+
+
+# data from https://pxdata.stat.fi/PxWeb/pxweb/en/StatFin/StatFin__vaerak/statfin_vaerak_pxt_11rd.px/table/tableViewLayout1/
+
+fin_pop <- read_csv(here::here("data", "001_11rd_2022_20230504-160652.csv"))
+
+fin_pop <- janitor::clean_names(fin_pop)
+
+# seems to just take the first two values which is what we want
+
+fin_pop_long <- 
+fin_pop |> 
+  select(-2) |> 
+  pivot_longer(cols = !year,
+               names_to = c("sex", "age"),
+               names_sep = "_",
+               values_to = "pop",
+               )
+
+fin_pop_long <- 
+fin_pop_long |> 
+  filter(year >= 1990,
+         sex != "total",
+         age != "total")
+
+# hard codes oldest age to be 120
+
+fin_ages <- 
+  finland |> 
+  distinct(age) |> 
+  filter(age != "Total") |> 
+  mutate(age_first = as.numeric(str_extract(age, "[0-9]+")),
+         age_last = str_extract(str_sub(age, 3, str_length(age)),
+                                "[0-9]+"),
+         age_last = case_when(
+           is.na(age_last) & as.numeric(age_first) != max(as.numeric(age_first)) ~ as.numeric(age_first),
+           is.na(age_last) & as.numeric(age_first) == max(as.numeric(age_first)) ~ 99,
+           TRUE ~ as.numeric(age_last)
+         ),
+         age_last = if_else(is.na(age_last), 120, age_last),
+         age_range = map2(age_first, age_last, ~ seq(.x, .y, 1))) |> 
+  unnest(age_range) |> 
+  rename(age_group = age,
+         age = age_range)
+
+# watch out for coding of sex
+
+fin_ages_agg <- 
+  left_join(fin_ages, fin_pop_long |> mutate(age = as.numeric(age))) |> 
+  filter(!is.na(age_group),
+         sex == "males") |> 
+  group_by(age_group, year) |> 
+  summarise(pop = sum(pop)) |> 
+  ungroup() |> 
+  rename(age = age_group)
+
+finland_pop <- finland |> left_join(fin_ages_agg)
+
+
+
+
+wid_dat_m <- 
+  finland |> 
+  pivot_wider(names_from = c(year),
+              values_from = convicted_number) |> 
+  filter(gender == "Males") |> 
+  select(-gender, -age)
+
+ages <- 
+  finland |> 
+  pivot_wider(names_from = c(year),
+              values_from = convicted_number) |> 
+  filter(gender == "Total") |> 
+  select(age) |> 
+  mutate(age = as.numeric(stringr::str_sub(age, 1, 2)))
+
+# need consistent gender/sex naming
+
+fin_m_pop <- 
+  finland_pop |> 
+  filter(gender == "Males") |> 
+  select(-convicted_number) |> 
+  pivot_wider(names_from = c(year),
+              values_from = pop) |> 
+  select(-gender, -age)
+
+
+fin_m_rate <- 
+  pclm2D(
+    x = ages$age,
+    y = as.data.frame(wid_dat_m),
+    nlast = 20,
+    offset = as.data.frame(fin_m_pop) / 1000,
+  )
+
+plot(fin_m_rate)
+
+fin_m_rate$fitted |> 
+  as.data.frame() |> 
+  tibble::rownames_to_column() |> 
+  as_tibble() |> 
+  mutate(age = as.numeric(stringr::str_sub(rowname, 2, 3))) |> 
+  pivot_longer(cols = `1990`:`2021`,
+               values_to = "n",
+               names_to = "year") |> 
+  filter(age < 50) |> 
+  ggplot(aes(x = as.numeric(year), y = age, fill = n)) +
+  geom_tile() +
+  geom_contour(aes(z = n)) +
+  coord_equal() +
+  scale_fill_viridis_c()
+
+
+fin_m_rate$fitted |> 
+  as.data.frame() |> 
+  tibble::rownames_to_column() |> 
+  as_tibble() |> 
+  mutate(age = as.numeric(stringr::str_sub(rowname, 2, 3))) |> 
+  pivot_longer(cols = `1990`:`2021`,
+               values_to = "n",
+               names_to = "year") |> 
+  filter(age < 50) |> 
+  ggplot(aes(x = age, y = n, group = year, colour = as.numeric(year))) +
+  geom_line()
